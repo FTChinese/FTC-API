16b97d4be1cc65a4dd9f0763d06c05c2fdf23739
wanbo <gewanbo@gmail.com>
2016-04-08 17:50:35 +0800

    Add two new seeds.

---
+++ src/main/scala/com/ftchinese/workers/Seeder_31003.scala
@@ -0,0 +1,107 @@
+package com.ftchinese.workers
+
+import com.wanbo.easyapi.server.database.MysqlDriver
+import com.wanbo.easyapi.server.lib.{EasyException, EasyOutput, ISeeder, Seeder}
+import org.slf4j.LoggerFactory
+
+
+/**
+ * MyFT save configurations.
+ * Created by wanbo on 2016/04/08.
+ */
+final class Seeder_31003 extends Seeder with ISeeder {
+
+    name = "31003"
+
+    driver = new MysqlDriver
+
+    private var _uuId: String = ""
+    private val _type: String = "myft"
+    private var _name: String = ""
+    private var _value: String = ""
+    private var _cmd: Int = 1
+
+    private val log = LoggerFactory.getLogger(classOf[Seeder_31003])
+
+    override def onHandle(seed: Map[String, Any]): EasyOutput = {
+
+        var dataList = List[Map[String, Any]]()
+
+        try {
+            val uuId: String = seed.getOrElse("uuid", "").toString
+            val inType: String = seed.getOrElse("type", "").toString
+            val inValue: String = seed.getOrElse("value", "").toString
+            val cmd: String = seed.getOrElse("cmd", "").toString
+
+            if(uuId.isEmpty)
+                throw new EasyException("50002")
+
+            if(uuId.length < 3 || uuId.length > 36)
+                throw new EasyException("20001")
+
+            if(inType.isEmpty || inValue.length > 40)
+                throw new EasyException("20001")
+
+            if(inType.isEmpty || inValue.length > 40)
+                throw new EasyException("20001")
+
+            if(cmd == "0")
+                _cmd = 0
+
+            _uuId = uuId.trim
+            _name = inType.trim
+            _value = inValue.trim
+
+
+            // Write to database.
+            dataList = onDBHandle()
+
+            fruits.oelement = fruits.oelement.updated("errorcode", "0")
+            fruits.odata = dataList
+        } catch {
+            case ee: EasyException =>
+                fruits.oelement = fruits.oelement.updated("errorcode", ee.getCode)
+            case e: Exception =>
+                log.error("Seeder has exception:", e)
+                fruits.oelement = fruits.oelement.updated("errorcode", "-1")
+                fruits.oelement = fruits.oelement.updated("errormsg", e.getMessage)
+        }
+
+        fruits
+    }
+    override protected def onDBHandle(): List[Map[String, String]] = {
+        val dataList = List[Map[String, String]]()
+
+        try {
+            val driver = this.driver.asInstanceOf[MysqlDriver]
+            val conn = driver.getConnector("user_db", writable = true)
+
+            var sql = ""
+            if(_cmd == 1){
+                sql = "INSERT `user_personal` (uuid, `type`, `name`, `value`) VALUES('%s', '%s', '%s', '%s',);".format(_uuId, _type, _name, _value)
+            } else {
+                sql = "UPDATE `user_personal` SET `status` = 0 WHERE `uuid`='%s' and `type`='%s' and `name`='%s' and `value`='%s';".format(_uuId, _type, _name, _value)
+            }
+
+            log.info("SQL--------:" + sql)
+
+            val ps = conn.prepareStatement(sql)
+            val num = ps.executeUpdate()
+
+            if(num < 1){
+                throw new Exception("Data write error.")
+            }
+
+            ps.close()
+
+            conn.close()
+
+        } catch {
+            case e: Exception =>
+                throw e
+        }
+
+        dataList
+    }
+
+}
---
+++ src/main/scala/com/ftchinese/workers/Seeder_31004.scala
@@ -0,0 +1,111 @@
+package com.ftchinese.workers
+
+import com.wanbo.easyapi.server.cache.CacheManager
+import com.wanbo.easyapi.server.database.MysqlDriver
+import com.wanbo.easyapi.server.lib.{EasyException, EasyOutput, ISeeder, Seeder}
+import org.slf4j.LoggerFactory
+
+
+/**
+ * Query MyFT configurations.
+ * Created by wanbo on 2016/04/08.
+ */
+final class Seeder_31004 extends Seeder with ISeeder {
+
+    name = "31004"
+
+    driver = new MysqlDriver
+
+    private var _uuId: String = ""
+    private val _type: String = "myft"
+
+    private val log = LoggerFactory.getLogger(classOf[Seeder_31004])
+
+    override def onHandle(seed: Map[String, Any]): EasyOutput = {
+
+        var dataList = List[Map[String, Any]]()
+
+        try {
+            val uuId: String = seed.getOrElse("uuid", "").toString
+
+            if(uuId.isEmpty)
+                throw new EasyException("50002")
+
+            if(uuId.length < 3 || uuId.length > 36)
+                throw new EasyException("20001")
+
+            _uuId = uuId.trim
+
+
+            // Cache
+            val cache_name = this.getClass.getSimpleName + _uuId
+
+            val cacher = new CacheManager(_conf, expire = 10)
+            val cacheData = cacher.cacheData(cache_name)
+
+            if (cacheData != null && cacheData.oelement.get("errorcode").get == "0" && !isUpdateCache) {
+                dataList = cacheData.odata
+                fruits.oelement = fruits.oelement + ("fromcache" -> "true") + ("ttl" -> cacher.ttl.toString)
+            } else {
+
+                dataList = onDBHandle()
+
+                if (dataList.size < 1)
+                    throw new EasyException("20100")
+                else {
+                    val cache_data = new EasyOutput
+                    cache_data.odata = dataList
+
+                    cache_data.oelement = cache_data.oelement.updated("errorcode", "0")
+                    cacher.cacheData(cache_name, cache_data)
+                }
+            }
+            cacher.close()
+
+            fruits.oelement = fruits.oelement.updated("errorcode", "0")
+            fruits.odata = dataList
+        } catch {
+            case ee: EasyException =>
+                fruits.oelement = fruits.oelement.updated("errorcode", ee.getCode)
+            case e: Exception =>
+                log.error("Seeder has exception:", e)
+                fruits.oelement = fruits.oelement.updated("errorcode", "-1")
+                fruits.oelement = fruits.oelement.updated("errormsg", e.getMessage)
+        }
+
+        fruits
+    }
+    override protected def onDBHandle(): List[Map[String, String]] = {
+        var dataList = List[Map[String, String]]()
+
+        try {
+            val driver = this.driver.asInstanceOf[MysqlDriver]
+            val conn = driver.getConnector("user_db", writable = true)
+
+            val sql = "SELECT `name`,`value` FROM `user_personal` WHERE `status` = 1 and `uuid`='%s' and `type`='%s';".format(_uuId, _type)
+
+            log.info("SQL--------:" + sql)
+
+            val ps = conn.prepareStatement(sql)
+            val rs = ps.executeQuery()
+
+            while (rs.next()){
+                var tmpMap = Map[String, String]()
+                tmpMap = tmpMap + ("type" -> rs.getString(1))
+                tmpMap = tmpMap + ("value" -> rs.getString(2))
+                dataList = dataList :+ tmpMap
+            }
+
+            ps.close()
+
+            conn.close()
+
+        } catch {
+            case e: Exception =>
+                throw e
+        }
+
+        dataList
+    }
+
+}
