cd09af1557483ec4d327ea6d7681b34d0c34923a
wanbo <gewanbo@gmail.com>
2016-07-28 11:56:19 +0800

    Make seed 61009 redirect to 61008

--- src/main/scala/com/ftchinese/workers/Seeder_61009.scala
+++ src/main/scala/com/ftchinese/workers/Seeder_61009.scala
@@ -34,74 +34,79 @@ final class Seeder_61009 extends Seeder with ISeeder {
 
         try {
 
-            val uuId = seed.getOrElse("uuid", "").toString
-            val cookieId = seed.getOrElse("cookieid", "").toString
+            val recommendData = manager.transform("61008", seed)
+            fruits.oelement = recommendData.oelement
+            fruits.odata = recommendData.odata
+            throw new EasyException(recommendData.oelement.get("errorcode").get)
 
-            if (uuId == "" && cookieId == "")
-                throw new EasyException("20001")
-
-            if (uuId == "") {
-
-                // Get uuid By cookieId.
-                val uidData = manager.transform("61001", Map("cookieid" -> cookieId))
-
-                if (uidData.oelement.get("errorcode").get == "0") {
-                    val tmpUUID = uidData.odata.last.getOrElse("uuid", "").toString
-                    if (tmpUUID != "")
-                        _primeKey = tmpUUID
-                    else
-                        _primeKey = cookieId
-                } else {
-                    _primeKey = cookieId
-                }
-            } else
-                _primeKey = uuId
-
-            // Cache
-            val cache_name = this.getClass.getSimpleName + _primeKey
-
-            val cacher = new CacheManager(conf = _conf, expire = cache_time)
-            val cacheData = cacher.cacheData(cache_name)
-
-            if (cacheData != null && (cacheData.oelement.get("errorcode").get == "0" || cacheData.oelement.get("errorcode").get == "20101") && !isUpdateCache) {
-
-                if(cacheData.oelement.getOrElse("errorcode", "-1") == "20101") {
-                    throw new EasyException("20101")
-                }
-
-                dataList = cacheData.odata
-                fruits.oelement = fruits.oelement + ("fromcache" -> "true") + ("ttl" -> cacher.ttl.toString)
-            } else {
-
-                if(isUpdateCache) {
-
-                    if(cacheData != null && cacheData.oelement.getOrElse("errorcode", "-1") == "0" && cacheData.oelement.getOrElse("ttl", "-1").toInt > 10){
-                        log.info("----------- No need to update cache, because expiry time more than 10s.")
-                    } else {
-                        log.info("----------- Ready to update cache.")
-                        updateCache(cacher, cache_name)
-                    }
-
-                } else {
-                    log.info("----------- Push a CacheUpdate message to queue.")
-                    MessageQ.push("UpdateCache", CacheUpdate(name, seed))
-
-                    // Write a cache data as the lock which can prevent more threads to update cache.
-                    val cache_data = new EasyOutput
-                    cache_data.odata = List[Map[String, Any]]()
-
-                    cache_data.oelement = cache_data.oelement.updated("errorcode", "20101")
-                    cacher.cacheData(cache_name, cache_data, 10)
-
-                    throw new EasyException("20101")
-                }
-
-            }
-            cacher.close()
-
-            fruits.oelement = fruits.oelement.updated("errorcode", "0").+("duration" -> (System.currentTimeMillis() - startRunTime).toString)
-            //fruits.odata = util.Random.shuffle(dataList).slice(0, 10)
-            fruits.odata = dataList.slice(0, 10)
+//            val uuId = seed.getOrElse("uuid", "").toString
+//            val cookieId = seed.getOrElse("cookieid", "").toString
+//
+//            if (uuId == "" && cookieId == "")
+//                throw new EasyException("20001")
+//
+//            if (uuId == "") {
+//
+//                // Get uuid By cookieId.
+//                val uidData = manager.transform("61001", Map("cookieid" -> cookieId))
+//
+//                if (uidData.oelement.get("errorcode").get == "0") {
+//                    val tmpUUID = uidData.odata.last.getOrElse("uuid", "").toString
+//                    if (tmpUUID != "")
+//                        _primeKey = tmpUUID
+//                    else
+//                        _primeKey = cookieId
+//                } else {
+//                    _primeKey = cookieId
+//                }
+//            } else
+//                _primeKey = uuId
+//
+//            // Cache
+//            val cache_name = this.getClass.getSimpleName + _primeKey
+//
+//            val cacher = new CacheManager(conf = _conf, expire = cache_time)
+//            val cacheData = cacher.cacheData(cache_name)
+//
+//            if (cacheData != null && (cacheData.oelement.get("errorcode").get == "0" || cacheData.oelement.get("errorcode").get == "20101") && !isUpdateCache) {
+//
+//                if(cacheData.oelement.getOrElse("errorcode", "-1") == "20101") {
+//                    throw new EasyException("20101")
+//                }
+//
+//                dataList = cacheData.odata
+//                fruits.oelement = fruits.oelement + ("fromcache" -> "true") + ("ttl" -> cacher.ttl.toString)
+//            } else {
+//
+//                if(isUpdateCache) {
+//
+//                    if(cacheData != null && cacheData.oelement.getOrElse("errorcode", "-1") == "0" && cacheData.oelement.getOrElse("ttl", "-1").toInt > 10){
+//                        log.info("----------- No need to update cache, because expiry time more than 10s.")
+//                    } else {
+//                        log.info("----------- Ready to update cache.")
+//                        updateCache(cacher, cache_name)
+//                    }
+//
+//                } else {
+//                    log.info("----------- Push a CacheUpdate message to queue.")
+//                    MessageQ.push("UpdateCache", CacheUpdate(name, seed))
+//
+//                    // Write a cache data as the lock which can prevent more threads to update cache.
+//                    val cache_data = new EasyOutput
+//                    cache_data.odata = List[Map[String, Any]]()
+//
+//                    cache_data.oelement = cache_data.oelement.updated("errorcode", "20101")
+//                    cacher.cacheData(cache_name, cache_data, 10)
+//
+//                    throw new EasyException("20101")
+//                }
+//
+//            }
+//            cacher.close()
+//
+//            fruits.oelement = fruits.oelement.updated("errorcode", "0").+("duration" -> (System.currentTimeMillis() - startRunTime).toString)
+//            //fruits.odata = util.Random.shuffle(dataList).slice(0, 10)
+//            fruits.odata = dataList.slice(0, 10)
         } catch {
             case ee: EasyException =>
                 fruits.oelement = fruits.oelement.updated("errorcode", ee.getCode).+("duration" -> (System.currentTimeMillis() - startRunTime).toString)
